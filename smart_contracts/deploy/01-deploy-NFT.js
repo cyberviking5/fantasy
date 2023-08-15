ee1ddac183b4397a0b6ba2d8047ba90fda155acd
cyberviking5 <aviral.hatwal@gmail.com>
2023-08-16 01:25:02 +0530

    Fund contract along with fund test along with some minor changes

--- smart_contracts/deploy/01-deploy-NFT.js
+++ smart_contracts/deploy/01-deploy-NFT.js
@@ -1,36 +1,25 @@
-//import
- //main function
- //calling main function
-//  async function deployFun(){
-//     console.log("hi");
-//  }
-//  module.exports.default=deployFun;
 const {networkConfig,developmentChains,}=require('../helper-hardhat-config.js')
 require("dotenv").config()
 const {network}=require('hardhat');
 const {verify}=require('../utils/verify.js')
 
 module.exports=async({getNamedAccounts,deployments})=>{
-    // const {getNamedAccounts,deployments}=hre;
     const {deploy,log}=deployments;
     const {deployer}=await getNamedAccounts();
     const chainId=network.config.chainId
-    // const ethUsdPriceAddress=networkConfig[chainId]["ethUsdPriceFeed"]
     const NFT=await deploy("NFT",{
         from:deployer,
         args:[],
         log:true,
-        waitConfirmation:network.config.blockConfirmation || 5,
+        waitConfirmation:network.config.blockConfirmation || 10,
     })
 
     if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY) {
         console.log("Waiting for block confirmations...")
-        // await NFT.deployTransaction.wait(6)
         await verify(NFT.address, [])
       }
     
 
     log("----------------------------------")
-    //when going for localhost or network we want to use mock
 }
 module.exports.tags=["all","NFT"]
--- smart_contracts/deploy/04-deploy-Fund.js
+++ smart_contracts/deploy/04-deploy-Fund.js
@@ -1,33 +1,26 @@
-//import
- //main function
- //calling main function
-//  async function deployFun(){
-//     console.log("hi");
-//  }
-//  module.exports.default=deployFun;
 const {networkConfig,developmentChains,}=require('../helper-hardhat-config.js')
 require("dotenv").config()
 const {network}=require('hardhat');
 const {verify}=require('../utils/verify.js')
 
 module.exports=async({getNamedAccounts,deployments})=>{
-    // const {getNamedAccounts,deployments}=hre;
     const {deploy,log}=deployments;
     const {deployer}=await getNamedAccounts();
     const chainId=network.config.chainId
-    // const ethUsdPriceAddress=networkConfig[chainId]["ethUsdPriceFeed"]
-    const SimpleFlashLoan=await deploy("SimpleFlashLoan",{
+    const Fund=await deploy("Fund",{
         from:deployer,
-        args:["0x0496275d34753A48320CA58103d5220d394FF77F"],
+        args:[],
         log:true,
         waitConfirmation:network.config.blockConfirmation || 10,
     })
+
     if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY) {
         console.log("Waiting for block confirmations...")
-        // await NFT.deployTransaction.wait(6)
-        await verify(SimpleFlashLoan.address, ["0x0496275d34753A48320CA58103d5220d394FF77F"])
+        await verify(Fund.address, [])
       }
+    
+
     log("----------------------------------")
     //when going for localhost or network we want to use mock
 }
-module.exports.tags=["all","SimpleFlashLoan"]
+module.exports.tags=["all","Fund"]
