38f4d784caae8db7f26492b819f006890f4f1959
smayank1214 <rawatmayank716@gmail.com>
2023-08-20 20:09:05 +0530

    Added links to social media icons and done small changes in Team page

--- frontend/src/pages/Team/Team.jsx
+++ frontend/src/pages/Team/Team.jsx
@@ -8,11 +8,13 @@ import {
   FaFacebook,
   FaInstagram,
   FaGithub,
-  FaBitcoin
+  FaBitcoin,
+  FaLinkedin
 } from 'react-icons/fa'
 import Footer from '../../components/Footer/Footer'
 import {motion} from 'framer-motion'
 import logo from '../../assets/logo.png'
+import { Link } from 'react-router-dom'
 
 const Team = () => {
   return (
@@ -34,7 +36,7 @@ const Team = () => {
           <div className='team-man'>
             <div className='team-img' id='av'><img src={aviral} alt="" /></div>
             <div className='tb'>
-              <div className='social-img'><FaInstagram size={40}/><FaFacebook size={40}/><FaGithub size={40}/></div>
+              <div className='social-img'><a href="https://www.instagram.com/aviral_hatwal/"><FaInstagram size={40}/></a><a href="https://www.linkedin.com/in/aviral-hatwal-1734b0258"><FaLinkedin size={40}/></a><a href="https://github.com/cyberviking5"><FaGithub size={40}/></a></div>
               <h1>AVIRAL HATWAL</h1>
               <span>BlockChain Enthusiast</span>
               <p>We as a team are very responsible and clear with our vision. Our mission, purpose, goals and values involved the creation of our vision. </p>
@@ -43,7 +45,7 @@ const Team = () => {
           <div className='team-man'>
             <div className='team-img' id='kd'><img src={kudnar} alt="" /></div>
             <div className='tb'>
-              <div className='social-img'><FaInstagram size={40}/><FaFacebook size={40}/><FaGithub size={40}/></div>
+            <div className='social-img'><Link to='/fb'><FaInstagram size={40}/></Link><a href="https://www.linkedin.com/in/yash-kudnar-64544a224"><FaLinkedin size={40}/></a><a href="https://github.com/Yash9276"><FaGithub size={40}/></a></div>
               <h1>YASH KUDNAR</h1>
               <span>BlockChain Enthusiast</span>
               <p>We as a team are very responsible and clear with our vision. Our mission, purpose, goals and values involved the creation of our vision. </p>
@@ -52,7 +54,7 @@ const Team = () => {
           <div className='team-man'>
             <div className='team-img' id='mr'><img src={mayank} alt="" /></div>
             <div className='tb'>
-              <div className='social-img'><FaInstagram size={40}/><FaFacebook size={40}/><FaGithub size={40}/></div>
+            <div className='social-img'><a href="https://www.instagram.com/smayank_12/"><FaInstagram size={40}/></a><a href="https://www.linkedin.com/in/mayank-rawat-8aa02a257"><FaLinkedin size={40}/></a><a href="https://github.com/smayank1214"><FaGithub size={40}/></a></div>
               <h1>MAYANK RAWAT</h1>
               <span>FRONTEND DEVELOPER</span>
               <p>We as a team are very responsible and clear with our vision. Our mission, purpose, goals and values involved the creation of our vision. </p>
